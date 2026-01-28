62f05b65ea55c85319b8783a1e4f4b277753a5f8
Yabasha <bashar.ayyash@gmail.com>
2026-01-28 16:19:10 +0300

    fix web lint by using eslint directly instead of removed next lint command

---
+++ apps/web/eslint.config.mjs
@@ -0,0 +1,18 @@
+import js from "@eslint/js";
+import tseslint from "typescript-eslint";
+
+export default tseslint.config(
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    ignores: [".next/**", "node_modules/**"],
+  },
+  {
+    rules: {
+      "@typescript-eslint/no-unused-vars": [
+        "error",
+        { argsIgnorePattern: "^_", varsIgnorePattern: "^_" },
+      ],
+    },
+  }
+);
